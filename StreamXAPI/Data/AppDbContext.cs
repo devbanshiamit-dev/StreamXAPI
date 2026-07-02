f04a8543a79eec29c357e432e32890e653ba166e
Amit “Devanshi” <devbanshiamit@gmail.com>
2026-07-02 21:48:10 +0530

    Configure Entity Framework Core with movie database schema

---
+++ StreamXAPI/Data/AppDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StreamXAPI.Models;
+
+namespace StreamXAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Composite Key
+            modelBuilder.Entity<MovieActor>()
+                .HasKey(ma => new { ma.MovieId, ma.ActorId });
+
+            // Movie Relationship
+            modelBuilder.Entity<MovieActor>()
+                .HasOne(ma => ma.Movie)
+                .WithMany(m => m.MovieActors)
+                .HasForeignKey(ma => ma.MovieId);
+
+            // Actor Relationship
+            modelBuilder.Entity<MovieActor>()
+                .HasOne(ma => ma.Actor)
+                .WithMany(a => a.MovieActors)
+                .HasForeignKey(ma => ma.ActorId);
+
+
+            // Composite Key
+            modelBuilder.Entity<MovieGenre>()
+                .HasKey(mg => new { mg.MovieId, mg.GenreId });
+
+            // Movie Relationship
+            modelBuilder.Entity<MovieGenre>()
+                .HasOne(mg => mg.Movie)
+                .WithMany(m => m.MovieGenres)
+                .HasForeignKey(mg => mg.MovieId);
+
+            // Genre Relationship
+            modelBuilder.Entity<MovieGenre>()
+                .HasOne(mg => mg.Genre)
+                .WithMany(g => g.MovieGenres)
+                .HasForeignKey(mg => mg.GenreId);
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<MovieGenre> MovieGenres { get; set; }
+        public DbSet<MovieActor> MovieActors { get; set; }
+    }
+}
---
+++ StreamXAPI/Models/Movie.cs
@@ -0,0 +1,71 @@
+﻿namespace StreamXAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string ThumbnailUrl { get; set; } = string.Empty;
+
+        public string BannerUrl { get; set; } = string.Empty;
+
+        public string VideoUrl { get; set; } = string.Empty;
+
+        public int Duration { get; set; } // Minutes
+
+        public double Rating { get; set; }
+
+        public ICollection<MovieGenre> MovieGenres { get; set; } = new List<MovieGenre>();
+
+        public ICollection<MovieActor> MovieActors { get; set; } = new List<MovieActor>();
+    }
+
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<MovieGenre> MovieGenres { get; set; } = new List<MovieGenre>();
+    }
+    public class MovieGenre
+    {
+        public int MovieId { get; set; }
+
+        public Movie Movie { get; set; } = null!;
+
+        public int GenreId { get; set; }
+
+        public Genre Genre { get; set; } = null!;
+    }
+
+    public class Actor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public ICollection<MovieActor> MovieActors { get; set; } = new List<MovieActor>();
+    }
+    public class MovieActor
+    {
+        public int MovieId { get; set; }
+
+        public Movie Movie { get; set; } = null!;
+
+        public int ActorId { get; set; }
+
+        public Actor Actor { get; set; } = null!;
+
+        public string CharacterName { get; set; } = string.Empty;
+    }
+}
