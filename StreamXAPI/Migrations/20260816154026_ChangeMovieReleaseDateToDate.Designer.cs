2e35802880923eeb58a959fecc7c87383563a988
Amit “Devanshi” <devbanshiamit@gmail.com>
2026-08-16 21:13:13 +0530

    New Migration

--- StreamXAPI/Migrations/20260816154026_ChangeMovieReleaseDateToDate.Designer.cs
+++ StreamXAPI/Migrations/20260816154026_ChangeMovieReleaseDateToDate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using StreamXAPI.Data;
@@ -11,9 +12,11 @@ using StreamXAPI.Data;
 namespace StreamXAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260816154026_ChangeMovieReleaseDateToDate")]
+    partial class ChangeMovieReleaseDateToDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
